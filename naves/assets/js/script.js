97785bd213447e8f7dd7f2122c51977229e9ee2a
caiowmp <contato.caiomp@gmail.com>
2022-07-13 14:21:41 -0300

    Função para o amigo se movimentar

--- naves/assets/js/script.js
+++ naves/assets/js/script.js
@@ -45,7 +45,8 @@ function start() {
     movejogador();
     moveinimigo1();
     moveinimigo2();
-	
+	moveamigo();
+
 	} // Fim da função loop()
 
     //Função que movimenta o fundo do jogo
@@ -148,7 +149,18 @@ function start() {
 		}
     } // Fim da função moveinimigo2()
 
-
+    function moveamigo() {
+	
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left",posicaoX+1);
+                    
+            if (posicaoX>906) {
+                
+            $("#amigo").css("left",0);
+                        
+            }
+    
+    } // fim da função moveamigo()
     
 
 } // Fim da função start
