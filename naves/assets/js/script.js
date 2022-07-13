f8869afc3088bbf0f3bb2e18ad7109fc212660c9
caiowmp <contato.caiomp@gmail.com>
2022-07-13 12:51:37 -0300

    Criando funções para mover o jogador (subir e descer o helicóptero)

--- naves/assets/js/script.js
+++ naves/assets/js/script.js
@@ -11,8 +11,26 @@ function start() {
 
     //Principais variáveis do jogo
 	
-	var jogo = {}
+	var jogo = {};
+	var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+    
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
 	
+	$(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+    });
+    
+    
+    $(document).keyup(function(e){
+       jogo.pressionou[e.which] = false;
+    });
+
 	//Game Loop
     //indica o tempo de execução da função loop
 	jogo.timer = setInterval(loop,30);
@@ -20,6 +38,7 @@ function start() {
 	function loop() {
 	
 	movefundo();
+    movejogador();
 	
 	} // Fim da função loop()
 
@@ -31,6 +50,32 @@ function start() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position",esquerda-1);
         
-        } // fim da função movefundo()
+    } // fim da função movefundo()
+
+    function movejogador() {
+	
+        //caso W seja pressionado
+        if (jogo.pressionou[TECLA.W]) {
+            //pega a posição atual
+            var topo = parseInt($("#jogador").css("top"));
+            //joga pra cima
+            $("#jogador").css("top",topo-10);
+            
+        }
+            
+        //caso S seja pressionado
+        if (jogo.pressionou[TECLA.S]) {           
+            //pega a posição atual
+            var topo = parseInt($("#jogador").css("top"));
+            //joga pra baixo
+            $("#jogador").css("top",topo+10);	
+        }
+            
+        //caso D seja pressionado
+        if (jogo.pressionou[TECLA.D]) {    
+            //Chama função Disparo	
+        }
+        
+    } // fim da função movejogador()
 
 } // Fim da função start
