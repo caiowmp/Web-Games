bb23017cb1143bc46f8bcb0d6610dc5b85603d71
caiowmp <contato.caiomp@gmail.com>
2022-07-13 12:39:26 -0300

    Fazendo o fundo do game se movimentar

--- naves/assets/js/script.js
+++ naves/assets/js/script.js
@@ -9,4 +9,28 @@ function start() {
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-}
+    //Principais variáveis do jogo
+	
+	var jogo = {}
+	
+	//Game Loop
+    //indica o tempo de execução da função loop
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+	} // Fim da função loop()
+
+    //Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+        
+        //pega a posiçãoa tual do fundo, armazena em equerda e da um passo pra traz
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position",esquerda-1);
+        
+        } // fim da função movefundo()
+
+} // Fim da função start
